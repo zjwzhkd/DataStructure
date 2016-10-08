01d784ed7d37da4a36bf66849c0b67c24acd9246
Keda Huang <huangkeda88@163.com>
2016-10-08 23:58:56 +0800

    建立工程模板

--- DataStructure/main.c
+++ DataStructure/main.c
@@ -7,6 +7,7 @@
 //
 
 #include <stdio.h>
+#include "LibCommon.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
---
+++ Library/Include/LibCommon.h
@@ -0,0 +1,25 @@
+//
+//  LibCommon.h
+//  DataStructure
+//
+//  Created by 黄可达 on 2016/10/8.
+//  Copyright © 2016年 hkd. All rights reserved.
+//
+
+#ifndef LibCommon_h
+#define LibCommon_h
+
+/* 头文件 ---------------------------------------------------------------------*/
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* 基本类型 --------------------------------------------------------------------*/
+typedef int32_t BasePos_t;
+
+/* 宏定义 ---------------------------------------------------------------------*/
+#define LIB_MALLOC  malloc
+#define LIB_FREE    free
+
+#endif /* LibCommon_h */
