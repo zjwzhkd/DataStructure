cf4b69c0a435382ec7a794c54eded4c203e370cd
Keda Huang <huangkeda88@163.com>
2016-10-08 20:15:02 +0800

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by 黄可达 on 2016/10/8.
+//  Copyright © 2016年 hkd. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
